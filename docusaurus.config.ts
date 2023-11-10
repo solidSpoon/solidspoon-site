33d3ac5573fbcbfe60cd563b9232220575b31293
solidSpoon <cedarrival@outlook.com>
2023-11-10 17:29:41 +0800

    logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,18 +5,18 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
     title: 'solidSpoon\'s site',
     tagline: 'Dinosaurs are cool',
-    favicon: 'img/favicon.ico',
+    favicon: '/img/logoo.svg',
 
     // Set the production url of your site here
-    url: 'https://your-docusaurus-site.example.com',
+    url: 'https://solidspoon.xyz',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'solidSpoon', // Usually your GitHub org/user name.
+    projectName: 'solidSpoon.github.io', // Usually your repo name.
 
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
@@ -25,8 +25,8 @@ const config: Config = {
     // useful metadata like html lang. For example, if your site is Chinese, you
     // may want to replace "en" with "zh-Hans".
     i18n: {
-        defaultLocale: 'en',
-        locales: ['en'],
+        defaultLocale: 'zh-Hans',
+        locales: ['zh-Hans'],
     },
 
     presets: [
@@ -38,14 +38,14 @@ const config: Config = {
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/solidSpoon/solidSpoon.github.io/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 blog: {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
                     editUrl:
-                        'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+                        'https://github.com/solidSpoon/solidSpoon.github.io/tree/main/packages/create-docusaurus/templates/shared/',
                 },
                 theme: {
                     customCss: './src/css/custom.css',
@@ -61,7 +61,7 @@ const config: Config = {
             title: 'Hide',
             logo: {
                 alt: 'My Site Logo',
-                src: 'img/logo.png',
+                src: 'img/logoo.svg',
             },
             items: [
                 {to: '/blog', label: 'Blog', position: 'left'},
@@ -76,7 +76,7 @@ const config: Config = {
                     label: 'DashPlayer',
                 },
                 {
-                    href: 'https://github.com/facebook/docusaurus',
+                    href: 'https://github.com/solidSpoon',
                     label: 'GitHub',
                     position: 'right',
                 },
@@ -84,48 +84,48 @@ const config: Config = {
         },
         footer: {
           style: 'dark',
-          links: [
-            {
-              title: 'Docs',
-              items: [
-                {
-                  label: 'Tutorial',
-                  to: '/docs/intro',
-                },
-              ],
-            },
-            {
-              title: 'Community',
-              items: [
-                {
-                  label: 'Stack Overflow',
-                  href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                },
-                {
-                  label: 'Discord',
-                  href: 'https://discordapp.com/invite/docusaurus',
-                },
-                {
-                  label: 'Twitter',
-                  href: 'https://twitter.com/docusaurus',
-                },
-              ],
-            },
-            {
-              title: 'More',
-              items: [
-                {
-                  label: 'Blog',
-                  to: '/blog',
-                },
-                {
-                  label: 'GitHub',
-                  href: 'https://github.com/facebook/docusaurus',
-                },
-              ],
-            },
-          ],
-          copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+          // links: [
+            // {
+            //   title: 'Docs',
+            //   items: [
+            //     {
+            //       label: 'Tutorial',
+            //       to: '/docs/intro',
+            //     },
+            //   ],
+            // },
+            // {
+            //   title: 'Community',
+            //   items: [
+            //     {
+            //       label: 'Stack Overflow',
+            //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            //     },
+            //     {
+            //       label: 'Discord',
+            //       href: 'https://discordapp.com/invite/docusaurus',
+            //     },
+            //     {
+            //       label: 'Twitter',
+            //       href: 'https://twitter.com/docusaurus',
+            //     },
+            //   ],
+            // },
+            // {
+            //   title: 'More',
+            //   items: [
+            //     {
+            //       label: 'Blog',
+            //       to: '/blog',
+            //     },
+            //     {
+            //       label: 'GitHub',
+            //       href: 'https://github.com/facebook/docusaurus',
+            //     },
+            //   ],
+            // },
+          // ],
+          copyright: `Copyright © ${new Date().getFullYear()} Hide - solidSpoon, Inc. Built with Docusaurus.`,
         },
         prism: {
             theme: prismThemes.github,
