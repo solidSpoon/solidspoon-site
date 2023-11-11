b9417fa59930a75eca9f75e7b90e09438d6f2d48
solidSpoon <cedarrival@outlook.com>
2023-11-11 11:48:41 +0800

    自定义博客侧边栏位

--- src/theme/BlogSidebar/Desktop/index.js
+++ src/theme/BlogSidebar/Desktop/index.js
@@ -1,40 +1,58 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import {translate} from '@docusaurus/Translate';
 import {useVisibleBlogSidebarItems} from '@docusaurus/theme-common/internal';
 import styles from './styles.module.css';
 import {twMerge} from "tailwind-merge";
+import {useLocation} from "@docusaurus/router";
+
 export default function BlogSidebarDesktop({sidebar}) {
-  const items = useVisibleBlogSidebarItems(sidebar.items);
-  return (
-    <aside className="col col--3">
-      <nav
-        className={clsx(styles.sidebar)}
-        aria-label={translate({
-          id: 'theme.blog.sidebar.navAriaLabel',
-          message: 'Blog recent posts navigation',
-          description: 'The ARIA label for recent posts in the blog sidebar',
-        })}>
-        <div className={clsx(styles.sidebarItemTitle, 'margin-bottom--md')}>
-          {sidebar.title}
-        </div>
-          <div className={twMerge('h-full w-full overflow-y-auto thin-scrollbar')}>
-              <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
-                  {items.map((item) => (
-                      <li key={item.permalink} className={styles.sidebarItem}>
-                          <Link
-                              isNavLink
-                              to={item.permalink}
-                              className={styles.sidebarItemLink}
-                              activeClassName={styles.sidebarItemLinkActive}>
-                              {item.title}
-                          </Link>
-                      </li>
-                  ))}
-              </ul>
-          </div>
-      </nav>
-    </aside>
-  );
+    const location = useLocation();
+    const currentRef = React.useRef();
+    console.log('useRouteContext1', location);
+    useEffect(() => {
+        if (currentRef.current) {
+            currentRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'nearest',
+            });
+        }
+
+    }, [sidebar])
+    const items = useVisibleBlogSidebarItems(sidebar.items);
+    return (
+        <aside className="col col--3">
+            <nav
+                className={clsx(styles.sidebar)}
+                aria-label={translate({
+                    id: 'theme.blog.sidebar.navAriaLabel',
+                    message: 'Blog recent posts navigation',
+                    description: 'The ARIA label for recent posts in the blog sidebar',
+                })}>
+                <div className={clsx(styles.sidebarItemTitle, 'margin-bottom--md')}>
+                    {sidebar.title}
+                </div>
+                <div
+                    className={twMerge('h-full w-full overflow-y-auto thin-scrollbar')}>
+                    <ul className={clsx(styles.sidebarItemList, 'clean-list')}>
+                        {items.map((item) => {
+                                const isActive = location.pathname === item.permalink;
+                                return <li key={item.permalink} className={styles.sidebarItem}>
+                                    <Link
+                                        ref={isActive ? currentRef : undefined}
+                                        isNavLink
+                                        to={item.permalink}
+                                        className={styles.sidebarItemLink}
+                                        activeClassName={styles.sidebarItemLinkActive}>
+                                        {item.title}
+                                    </Link>
+                                </li>
+                            }
+                        )}
+                    </ul>
+                </div>
+            </nav>
+        </aside>
+    );
 }
